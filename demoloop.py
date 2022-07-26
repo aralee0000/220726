cdf9d3e624107506dfabc7d92c64ed9780284269
aralee <ara36912@naver.com>
2022-07-26 11:22:49 +0900

    리스트

--- demoloop.py
+++ demoloop.py
@@ -1,9 +1,43 @@
 # DemoLoop.py
 #초기값
+
 value=5
 while value > 0:
     print(value)
     value -=1
 
     print("------반복구문---")
+
+
+
+d = {"apple":100, "kiwi":200, "orange":300}
+for item in d.items():
+    print(item)
+
+for i in[2,3,4,5,6]:
+        print("---{0}단---".format(i))
+        for j in [1,2,3,4,5,6,7,8,9,]:
+            print("{0} * {1} = {2}".format(i, j, i*j))
+
+print("----break구문----")
+lst =[1,2,3,4,5,6,7,8,9,10]
+for i in lst:
+    #다중라인 주석처리 ctrl /
+    # if i > 5:
+    #     break
+    print("Item:{0}".format(i))
+
+print("----continue구문----")
+lst =[1,2,3,4,5,6,7,8,9,10]
+for i in lst:
+    if i % 2 ==0:
+        continue
+    print("Item:{0}".format(i))
+
+    #수열: 규칙이 있는 숫자의 열
+    print(list(range(5)))
+    print(list(range(1,11)))
+    print(list(range(2000,2023)))
+
+print(list(range(10,0,-1)))
 
